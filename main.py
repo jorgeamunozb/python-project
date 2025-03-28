c74dd53885c2034c5e531bd374a9639d454efaae
Darlink <darlinkaguilargr@gmail.com>
2025-03-28 18:54:48 -0500

    Leectura de archivo

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from sistema import Sistema
 import numpy as np
 
 sistema = Sistema(materias_capacity=20, usuarios_capacity=100, registros_capacity=200)
+
+try:
+    file1 = open("materias.txt", "r")
+    lineas = file1.readlines() 
+    print(lineas) 
+except:
+    print("Error al abrir el archivo")
+    
+file1.close()
